034f2e656d69dcb5cefc5526a50c16742dc1c2f6
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-06-26 12:31:43 +0530

    Created Action of sending details

---
+++ assert/lib/setting.py
@@ -0,0 +1,7 @@
+# EMAIL CONFIGURATION (for contact form)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '<your_email>'
+EMAIL_HOST_PASSWORD = 'your_password>'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
